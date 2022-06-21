368e67669e1f8a7b730b8f20be7cb93b83b76493
Safira Tyas Wandita <safeeratyas@gmail.com>
2022-06-21 23:25:17 +0700

    feat:add form for product info

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Homepage from "./pages/Homepage";
 import { Provider } from "react-redux";
-import Profile from './components/Profile/Form'
+import Profile from './pages/Profile'
+import ProductInfo from './pages/ProductInfo'
 import SellerProductPage from './pages/SellerProduct'
 import BuyerProductPage from './pages/BuyerProduct'
 
@@ -21,6 +22,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path='/profile' element={<Profile />} />
+          <Route path='/seller/sell' element={<ProductInfo />} />
           <Route path='/seller/product/:id' element={<SellerProductPage />} />
           <Route path='/buyer/product/:id' element={<BuyerProductPage />} />
         </Routes>
