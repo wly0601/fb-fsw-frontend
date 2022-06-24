8ca5ac8a25f0d88203f8a96f04a488eb761670c9
ghifari1209 <jumpraffi1@gmail.com>
2022-06-24 16:52:01 +0700

    feat:navbar-dropdown

--- src/components/Navbar/NavbarLogIn.js
+++ src/components/Navbar/NavbarLogIn.js
@@ -1,15 +1,24 @@
-import React from 'react'
-import { Navbar, Container,Nav, Form} from 'react-bootstrap'
+import React,{useState} from 'react'
+import { Navbar, Container,Nav, Form, NavItem, NavDropdown,Button, NavLink} from 'react-bootstrap'
 import {FaSearch} from 'react-icons/fa';
 import './Navigation.Module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fontsource/poppins';
-import {FaSignInAlt} from 'react-icons/fa';
 import {FaBell} from 'react-icons/fa'
 import {FaRegUser} from 'react-icons/fa';
 import {FaListUl} from 'react-icons/fa';
 
+const options = ["Safira", "Bagus", "Ghifari"];
+
 export default function NavbarLogIn() {
+    const [isOpen, setIsOpen] = useState(false);
+  
+    const toggling = () => setIsOpen(!isOpen);
+  
+    const onOptionClicked = ()=> () => {
+      setIsOpen(false);
+    };
+
        return (  
                  <Navbar className="ms-auto navbar"bg="white" expand="lg">
                     <Container fluid className='me-3'>
@@ -27,13 +36,28 @@ export default function NavbarLogIn() {
                             <i className='searchIcon' type='button'><FaSearch/></i>
                         </Form>
                         <Nav className="flex-grow-1 justify-content-end nav">
-                        <Nav.Link href="#link"><FaListUl className='link'/></Nav.Link>
+                        <Nav.Link href="#link"><FaListUl className='link'/>
+                        </Nav.Link>
                             <Nav.Link href="#home" >
                                 <FaBell className='link'/>
                             </Nav.Link>
-                            <Nav.Link href="#link">
-                                <FaRegUser className='link'/>
-                            </Nav.Link>              
+                          
+                            <NavLink >
+                                 <i type = 'button' className='button-fa' onClick={toggling}>
+                                 <FaRegUser className='link'/>
+                                 </i>
+                                {isOpen && (
+                           <div >
+                            <ul >
+                               {options.map(option => (
+                             <NavItem onClick={onOptionClicked(option)} key={Math.random()}>
+                              {option}
+                            </NavItem>
+                             ))}
+                            </ul>
+                          </div>
+                                 )}
+                         </NavLink>          
                         </Nav>
                     </Navbar.Collapse>
              </Container>
