032b32d27ee522bbf0745104724778c4aedfc053
Safira Tyas Wandita <safeeratyas@gmail.com>
2022-06-27 21:37:02 +0700

    feat: edit carousel in homepage

--- src/components/Organisms/Homepage/Home.js
+++ src/components/Organisms/Homepage/Home.js
@@ -1,6 +1,7 @@
 import Carousel from '../../Moleculs/Carousel/CarouselHomepage';
 import ButtonCategory from '../../Atoms/Button/ButtonCategory';
 import ItemCard from '../../Moleculs/Card/ItemCard';
+import BtnAddProduct from '../../Atoms/Button/BtnAddProduct';
 // import ButtonList from '../../Atoms/Button/ButtonList';
 import { IMAGES } from '../../../data/data';
 import { Container, Row, Col } from 'react-bootstrap';
@@ -9,33 +10,84 @@ import './Home.Module.css';
 const Home = () => {
   return (
     <>
-      <Container>
-        <Row>
-          <Col>
+      <Container fluid>
+        <div className="row mt-3">
+          <div className="col">
             <Carousel images={IMAGES} />
-          </Col>
-        </Row>
-        <Row className='mt-5'>
-          <Col>
-            <h5 style={{ textAlign: 'left', fontWeight: 'bold' }}>Telusuri Kategori</h5>
-          </Col>
-        </Row>
-        <Row className='mt-3'>
+          </div>
+        </div>
+        <div className="row mt-5 mx-5">
+          <div className="col-12">
+            <h5 style={{ textAlign: "left" }}>Telusuri Kategori</h5>
+          </div>
+        </div>
+        <div className="row mt-3 mx-5">
           <ButtonCategory />
-        </Row>
-        <Row className='mt-5'>
-          {Array.from({ length: 4 }).map((_, idx) => (
-            <Col>
-              <ItemCard
-                title='Jam Tangan Casio'
-                type='Aksesoris'
-                price='Rp 250.000'
-                image='https://placeimg.com/165/100/any'
-                imageAlt='Gambar jam tangan'
-              />
-            </Col>
-          ))}
-        </Row>
+        </div>
+        <div className="row mt-3 mx-5">
+          <div className="col-2">
+            <ItemCard
+              title="Jam Tangan Casio"
+              type="Aksesoris"
+              price="Rp 250.000"
+              image="https://placeimg.com/165/100/any"
+              imageAlt="Gambar jam tangan"
+            />
+          </div>
+          <div className="col-2">
+            <ItemCard
+              title="Jam Tangan Casio"
+              type="Aksesoris"
+              price="Rp 250.000"
+              image="https://placeimg.com/165/100/any"
+              imageAlt="Gambar jam tangan"
+            />
+          </div>
+          <div className="col-2">
+            <ItemCard
+              title="Jam Tangan Casio"
+              type="Aksesoris"
+              price="Rp 250.000"
+              image="https://placeimg.com/165/100/any"
+              imageAlt="Gambar jam tangan"
+            />
+          </div>
+          <div className="col-2">
+            <ItemCard
+              title="Jam Tangan Casio"
+              type="Aksesoris"
+              price="Rp 250.000"
+              image="https://placeimg.com/165/100/any"
+              imageAlt="Gambar jam tangan"
+            />
+          </div>
+          <div className="col-2">
+            <ItemCard
+              title="Jam Tangan Casio"
+              type="Aksesoris"
+              price="Rp 250.000"
+              image="https://placeimg.com/165/100/any"
+              imageAlt="Gambar jam tangan"
+            />
+          </div>
+          <div className="col-2">
+            <ItemCard
+              title="Jam Tangan Casio"
+              type="Aksesoris"
+              price="Rp 250.000"
+              image="https://placeimg.com/165/100/any"
+              imageAlt="Gambar jam tangan"
+            />
+          </div>
+        </div>
+        <div className="row mt-3 mb-3 mx-5">
+          <BtnAddProduct />
+        </div>
+
+        {/* <Row style={{ marginleft: 0}}>
+          <Col style={{ paddingleft: 0}}><ImageLoginRegis /></Col>
+          <Col><LoginForm /></Col>
+        </Row> */}
       </Container>
     </>
   )
