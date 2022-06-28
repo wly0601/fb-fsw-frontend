6d20686d64f2b09d9ee4b269e430bbf2b1d18161
Tubagus Mochammad Eza <tubagusm.eza@gmail.com>
2022-06-28 21:38:25 +0700

    feat: integrate login page with backend

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {
   Routes,
   Route,
 } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Homepage from './pages';
 import Register from './pages/Register';
 import Login from './pages/Login';
@@ -12,21 +13,24 @@ import SellerProduct from './pages/SellerProduct';
 import BuyerProduct from './pages/BuyerProduct';
 import ListProduct from './pages/ListProduct';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import store from './store';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<InfoProfile />} />
-        <Route path="/seller/sell" element={<InfoProduct />} />
-        <Route path="/seller/product/:id" element={<SellerProduct />} />
-        <Route path="/buyer/product/:id" element={<BuyerProduct />} />
-        <Route path="/side" element={<ListProduct />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<InfoProfile />} />
+          <Route path="/seller/sell" element={<InfoProduct />} />
+          <Route path="/seller/product/:id" element={<SellerProduct />} />
+          <Route path="/buyer/product/:id" element={<BuyerProduct />} />
+          <Route path="/side" element={<ListProduct />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
