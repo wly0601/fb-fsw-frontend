fc531de91ca69faba80c4f3b8fdf86c5aaa183d2
Safira Tyas Wandita <safeeratyas@gmail.com>
2022-07-07 22:14:23 +0700

    fix: bugs

--- src/components/Organisms/Navbar/NavbarLogin.js
+++ src/components/Organisms/Navbar/NavbarLogin.js
@@ -3,24 +3,21 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, Navigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faList } from '@fortawesome/free-solid-svg-icons';
 import {
-  Navbar,
-  Container,
-  Nav,
-  Form,
-  NavLink,
-  NavDropdown,
-  Button,
+  Navbar, Container, Nav, Form, Row, Col, Toast, Button, ToastContainer, Dropdown,
 } from 'react-bootstrap';
 import {
-  FaSearch, FaBell, FaRegUser, FaListUl, FaSignInAlt,
+  FaSearch, FaBell, FaRegUser, FaListUl,
 } from 'react-icons/fa';
 import { logout } from '../../../actions/auth';
-
+import CardNotification from '../../Moleculs/Card/CardNotification';
 import './Navigation.Module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function NavbarLogin() {
+  const [show, setShow] = useState(false);
   const userLogin = useSelector((state) => { return state.auth; });
   const { user } = userLogin;
   const dispatch = useDispatch();
@@ -30,50 +27,111 @@ function NavbarLogin() {
     window.location.href = '/';
   };
   return (
-    <Navbar className="ms-auto navbar" bg="white" expand="lg">
-      <Container fluid className="me-3">
-        <Navbar.Brand href="#home" className="logo" />
-
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Form className="d-flex test">
-            <input
-              type="search"
-              placeholder="Cari di sini..."
-              className="me-2 searchBar"
-              aria-label="Search"
-            />
-            <i className="searchIcon" type="button"><FaSearch /></i>
-          </Form>
-          <Nav className="flex-grow-1 justify-content-end nav">
-            <Nav.Link href="#link"><FaListUl className="link" /></Nav.Link>
-            <Nav.Link href="#home">
-              <FaBell className="link" />
-            </Nav.Link>
-            <NavLink>
-              <i type="button" className="button-fa">
+    <>
+      <Navbar className="ms-auto navbar" bg="white" expand="lg">
+        <Container fluid className="me-3">
+          <Navbar.Brand href="#home" className="logo" />
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Form className="d-flex test">
+              <input
+                type="search"
+                placeholder="Cari di sini..."
+                className="me-2 searchBar"
+                aria-label="Search"
+              />
+              <i className="searchIcon" type="button"><FaSearch /></i>
+            </Form>
+            <Nav className="flex-grow-1 justify-content-end nav">
+              <Dropdown className="mt-1">
+                <Dropdown.Toggle
+                  className="btn-toggle"
+                  style={{
+                    backgroundColor: 'transparent',
+                    color: 'black',
+                    border: 'none',
+                    zIndex: '1',
+                  }}
+                >
+                  <FontAwesomeIcon
+                    icon={faList}
+                    style={{
+                      zIndex: 2,
+                    }}
+                  />
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  <Dropdown.Item href="../history/seller">History Seller</Dropdown.Item>
+                  <Dropdown.Item href="../history/buyer">History Buyer</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+              <Button
+                onClick={() => { return setShow(true); }}
+                style={{
+                  backgroundColor: 'transparent',
+                  border: 'none',
+                  overflow: 'hidden',
+                }}
+              >
+                <FaBell className="link" />
+              </Button>
+              <Dropdown className="mt-1">
+                <Dropdown.Toggle
+                  className="btn-toggle"
+                  style={{
+                    backgroundColor: 'transparent',
+                    color: 'black',
+                    border: 'none',
+                    zIndex: '1',
+                  }}
+                >
+                  <FaRegUser className="link" />
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  <Dropdown.Item href="../history/seller">Profile</Dropdown.Item>
+                  <Dropdown.Item onClick={logOut}>Logout</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+              {/* <Nav.Link href="../../profile">
                 <FaRegUser className="link" />
-              </i>
-            </NavLink>
-            <NavDropdown>
-              <Nav.Link>
-                <Link to="/profile">
-                  Profile
-                </Link>
-              </Nav.Link>
-              <Nav.Link>
-                <Link to="/seller/sell">
-                  Daftar Jual
-                </Link>
-              </Nav.Link>
-              <Nav.Link onClick={logOut}>
-                Logout
-              </Nav.Link>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+              </Nav.Link> */}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Row>
+        <Col xs={10}>
+          <ToastContainer position="top-end" className="notification p-3">
+            <Toast position="top-end" onClose={() => { return setShow(false); }} show={show} delay={3000} autohide>
+              <Toast.Header>
+                <img
+                  src="holder.js/20x20?text=%20"
+                  className="rounded me-2"
+                  alt=""
+                />
+                <small>08 Jul, 20.04</small>
+              </Toast.Header>
+              <Toast.Body>
+                {Array.from({ length: 4 }).map((_, idx) => {
+                  return (
+                    <Col xs={12}>
+                      <CardNotification
+                        title="Penawaran Produk"
+                        name="Jam Tangan Casio"
+                        price="Rp.250.000"
+                        offering="Ditawar 200.000"
+                        image="https://placeimg.com/165/100/any"
+                        imageAlt="Gambar jam tangan"
+                      />
+                    </Col>
+                  );
+                })}
+              </Toast.Body>
+            </Toast>
+          </ToastContainer>
+        </Col>
+      </Row>
+    </>
   );
 }
 
