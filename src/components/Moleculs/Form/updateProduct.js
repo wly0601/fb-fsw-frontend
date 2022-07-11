cb5b9b384423e63b83a73645181c1a5a14293065
Safira Tyas Wandita <safeeratyas@gmail.com>
2022-07-11 20:01:58 +0700

    fix: eslint

--- src/components/Moleculs/Form/updateProduct.js
+++ src/components/Moleculs/Form/updateProduct.js
@@ -13,7 +13,7 @@ const required = (value) => {
   if (!value) {
     return (
       <div className="alert alert-danger" role="alert">
-        This field is required!
+        This field is required!!!
       </div>
     );
   }
