669e94353a7e59145ba666b7936f9e93f7cfb69b
Tubagus Mochammad Eza <tubagusm.eza@gmail.com>
2022-07-14 22:38:47 +0700

    style: offering page responsive interface

--- src/components/Moleculs/Card/CardAcceptOffering.js
+++ src/components/Moleculs/Card/CardAcceptOffering.js
@@ -21,28 +21,28 @@ function CardHistory(props) {
       <Title title="Daftar Produkmu Yang Ditawar" />
       <Card className="card-items-offering" style={{ borderRadius: '16px' }}>
         <Row>
-          <Col xs={2}>
+          <Col xs={4}>
             <Card.Img src={image} className="mt-4 mx-3 card-img-top image-offering" alt={imageAlt} />
           </Col>
-          <Col xs={10}>
+          <Col xs={8}>
             <Card.Body>
               <Card.Title style={{ fontSize: '10px', color: 'grey' }}>{title}</Card.Title>
               <Card.Title>{name}</Card.Title>
               <Card.Text>{price}</Card.Text>
               <Card.Text>{offering}</Card.Text>
             </Card.Body>
-            <div className="justify-content-end button-align">
-              <VerticalModals
-                show={modalShow}
-                onHide={() => { return setModalShow(false); }}
-              />
-              <Button variant="secondary" className="button-accept">
-                Hubungi di
-                {' '}
-                <FaWhatsapp />
-              </Button>
-            </div>
           </Col>
+          <div className="justify-content-end button-align">
+            <VerticalModals
+              show={modalShow}
+              onHide={() => { return setModalShow(false); }}
+            />
+            <Button variant="secondary" className="button-accept" style={{ fontSize: '15px' }}>
+              Hubungi
+              {' '}
+              <FaWhatsapp />
+            </Button>
+          </div>
         </Row>
       </Card>
     </Container>
